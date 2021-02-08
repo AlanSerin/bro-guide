4a4188bd97b9763a701a2ffc3bdcfac66b1d491c
Alan Serin <alanserin114@gmail.com>
2021-02-08 23:48:52 +0200

    Updates

---
+++ precache-manifest.93e143198d94665bb1b379aec5ad7749.js
@@ -0,0 +1,318 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4befa3f5a429dc976e46afd56c78d43e",
+    "url": "/bro-guide/index.html"
+  },
+  {
+    "revision": "be2d670385716b025d3b",
+    "url": "/bro-guide/static/css/6.3614eac3.chunk.css"
+  },
+  {
+    "revision": "f18e5fe50b72fea24068",
+    "url": "/bro-guide/static/css/main.ae2270d2.chunk.css"
+  },
+  {
+    "revision": "8cb73e19f0143371b06f",
+    "url": "/bro-guide/static/js/0.adf6f41a.chunk.js"
+  },
+  {
+    "revision": "568f251a378c470e2ef0",
+    "url": "/bro-guide/static/js/10.d65b306a.chunk.js"
+  },
+  {
+    "revision": "a9fe2b8620e1ace7e351",
+    "url": "/bro-guide/static/js/11.d333d14f.chunk.js"
+  },
+  {
+    "revision": "9c3354c24f81f9af4d6e",
+    "url": "/bro-guide/static/js/12.411fbe57.chunk.js"
+  },
+  {
+    "revision": "152f61ba606cc21e5238",
+    "url": "/bro-guide/static/js/13.aabe1fef.chunk.js"
+  },
+  {
+    "revision": "8348b48cc07c88a671ca",
+    "url": "/bro-guide/static/js/14.0101940b.chunk.js"
+  },
+  {
+    "revision": "db93966a790c33e035f2",
+    "url": "/bro-guide/static/js/15.222d9d0f.chunk.js"
+  },
+  {
+    "revision": "6e2b2184b61ac55de658",
+    "url": "/bro-guide/static/js/16.e1d13594.chunk.js"
+  },
+  {
+    "revision": "4dcbcc14ab438e7e1586",
+    "url": "/bro-guide/static/js/17.b2cbeb92.chunk.js"
+  },
+  {
+    "revision": "864f070cb8cc11c4f497",
+    "url": "/bro-guide/static/js/18.b1edd8e2.chunk.js"
+  },
+  {
+    "revision": "8c9bdb7b1c9ef3a0e19d",
+    "url": "/bro-guide/static/js/19.723e2e66.chunk.js"
+  },
+  {
+    "revision": "54d35bf94b9158d20509",
+    "url": "/bro-guide/static/js/20.8feb8e7b.chunk.js"
+  },
+  {
+    "revision": "1446fa3d03c752c69336",
+    "url": "/bro-guide/static/js/21.0f68604c.chunk.js"
+  },
+  {
+    "revision": "c5a76c1676b6ecad72ed",
+    "url": "/bro-guide/static/js/22.cebc2e1b.chunk.js"
+  },
+  {
+    "revision": "eff00e9e040df5e0e473",
+    "url": "/bro-guide/static/js/23.4f22fd65.chunk.js"
+  },
+  {
+    "revision": "bbac5f4abb5b91e3e4c4",
+    "url": "/bro-guide/static/js/24.866ec0fb.chunk.js"
+  },
+  {
+    "revision": "8a0f78775ffe0324ab97",
+    "url": "/bro-guide/static/js/25.6079aab4.chunk.js"
+  },
+  {
+    "revision": "b38b085dd232f2245889",
+    "url": "/bro-guide/static/js/26.7de5097e.chunk.js"
+  },
+  {
+    "revision": "ca66ca3718e5230702d4",
+    "url": "/bro-guide/static/js/27.370cd11d.chunk.js"
+  },
+  {
+    "revision": "44f5bcb072d0e83efc74",
+    "url": "/bro-guide/static/js/28.258c29ba.chunk.js"
+  },
+  {
+    "revision": "75c3e991a28028825716",
+    "url": "/bro-guide/static/js/29.d9c53ce0.chunk.js"
+  },
+  {
+    "revision": "fa34955c30acce2ce3a5",
+    "url": "/bro-guide/static/js/30.d5304aed.chunk.js"
+  },
+  {
+    "revision": "fa8ac4dc7a1044ab9a9f",
+    "url": "/bro-guide/static/js/31.efc57a1d.chunk.js"
+  },
+  {
+    "revision": "dee18c84d9db79a01157",
+    "url": "/bro-guide/static/js/32.ebcca47f.chunk.js"
+  },
+  {
+    "revision": "09e460f4f2d407f64793",
+    "url": "/bro-guide/static/js/33.ea65ceac.chunk.js"
+  },
+  {
+    "revision": "15a7844ded8f51254921",
+    "url": "/bro-guide/static/js/34.8de26c8f.chunk.js"
+  },
+  {
+    "revision": "5f455b12b536d11a3bce",
+    "url": "/bro-guide/static/js/35.ea22ab28.chunk.js"
+  },
+  {
+    "revision": "765e55dcc153818d78a8",
+    "url": "/bro-guide/static/js/36.666aa07c.chunk.js"
+  },
+  {
+    "revision": "86d41c0cbc49dbd39c96",
+    "url": "/bro-guide/static/js/37.491c0e01.chunk.js"
+  },
+  {
+    "revision": "56303232862acb48576d",
+    "url": "/bro-guide/static/js/38.ca766929.chunk.js"
+  },
+  {
+    "revision": "5cc89ff116ff0e830ef6",
+    "url": "/bro-guide/static/js/39.fbc65413.chunk.js"
+  },
+  {
+    "revision": "cbb643a2770b909b0ed6",
+    "url": "/bro-guide/static/js/40.0ba23d83.chunk.js"
+  },
+  {
+    "revision": "f6cea75d3e27428565f0",
+    "url": "/bro-guide/static/js/41.d9aaad33.chunk.js"
+  },
+  {
+    "revision": "14d02b1b2eaada097c54",
+    "url": "/bro-guide/static/js/42.61e1e4a9.chunk.js"
+  },
+  {
+    "revision": "cb479931c3d55740b304",
+    "url": "/bro-guide/static/js/43.619b2557.chunk.js"
+  },
+  {
+    "revision": "7bdf2663dd8adf835ea6",
+    "url": "/bro-guide/static/js/44.89b0a40e.chunk.js"
+  },
+  {
+    "revision": "7f2f19556ff49a26f3b2",
+    "url": "/bro-guide/static/js/45.8dba27a4.chunk.js"
+  },
+  {
+    "revision": "5d8785b5e3fb4d34904b",
+    "url": "/bro-guide/static/js/46.ce9f7f06.chunk.js"
+  },
+  {
+    "revision": "4a5198782387330c4413",
+    "url": "/bro-guide/static/js/47.d919f1c8.chunk.js"
+  },
+  {
+    "revision": "2aab327b9fb659db965a",
+    "url": "/bro-guide/static/js/48.d125ccfb.chunk.js"
+  },
+  {
+    "revision": "5e02a58bc0fd21919df0",
+    "url": "/bro-guide/static/js/49.b3d77851.chunk.js"
+  },
+  {
+    "revision": "e4328846173cf6174e24",
+    "url": "/bro-guide/static/js/50.080b2fe7.chunk.js"
+  },
+  {
+    "revision": "8415d65579c03e198c24",
+    "url": "/bro-guide/static/js/51.4d3e03dc.chunk.js"
+  },
+  {
+    "revision": "cbb7062559330954d3fb",
+    "url": "/bro-guide/static/js/52.abddd394.chunk.js"
+  },
+  {
+    "revision": "1f5f0a79d02fb43f21dc",
+    "url": "/bro-guide/static/js/53.f8dc65eb.chunk.js"
+  },
+  {
+    "revision": "c074a4ccafd4f8545e24",
+    "url": "/bro-guide/static/js/54.50b8af7a.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/bro-guide/static/js/54.50b8af7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bf5532f80c5b066a008",
+    "url": "/bro-guide/static/js/55.5b2a5c75.chunk.js"
+  },
+  {
+    "revision": "0c2f507147f30e357cf8",
+    "url": "/bro-guide/static/js/56.b971443e.chunk.js"
+  },
+  {
+    "revision": "d1f6f3ef1f41f54c6e76",
+    "url": "/bro-guide/static/js/57.1065615c.chunk.js"
+  },
+  {
+    "revision": "7fc8853dccb06bd40c05",
+    "url": "/bro-guide/static/js/58.82ea4a53.chunk.js"
+  },
+  {
+    "revision": "d70c965e26fc9c576e07",
+    "url": "/bro-guide/static/js/59.7c46627e.chunk.js"
+  },
+  {
+    "revision": "be2d670385716b025d3b",
+    "url": "/bro-guide/static/js/6.87ef2d42.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/bro-guide/static/js/6.87ef2d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96a41fe7a476dd1128c5",
+    "url": "/bro-guide/static/js/60.fd20962f.chunk.js"
+  },
+  {
+    "revision": "0928590f8a87f84af606",
+    "url": "/bro-guide/static/js/61.ad061557.chunk.js"
+  },
+  {
+    "revision": "e2cb397828810577c030",
+    "url": "/bro-guide/static/js/62.0deee5f4.chunk.js"
+  },
+  {
+    "revision": "f5029dfb31b543319e21",
+    "url": "/bro-guide/static/js/63.f55c72d0.chunk.js"
+  },
+  {
+    "revision": "ae0a5e8f2a9fb11e706b",
+    "url": "/bro-guide/static/js/64.293be319.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/bro-guide/static/js/64.293be319.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72d438d3cde863fe546d",
+    "url": "/bro-guide/static/js/65.1850ec83.chunk.js"
+  },
+  {
+    "revision": "a45d7d389e37f44dafac",
+    "url": "/bro-guide/static/js/66.cfae7835.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/bro-guide/static/js/66.cfae7835.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "006021fc3bfff6bbfa1b",
+    "url": "/bro-guide/static/js/67.da78ff0f.chunk.js"
+  },
+  {
+    "revision": "8202024d85d922c9e604",
+    "url": "/bro-guide/static/js/68.2b06d884.chunk.js"
+  },
+  {
+    "revision": "383899e60ed69a9a53414c6836d91ab5",
+    "url": "/bro-guide/static/js/68.2b06d884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3237e52832c0b04ec0ea",
+    "url": "/bro-guide/static/js/7.36af994e.chunk.js"
+  },
+  {
+    "revision": "bb79913d2f94a058ddf7",
+    "url": "/bro-guide/static/js/8.208682a6.chunk.js"
+  },
+  {
+    "revision": "ae3c46ade2bd6133b7e1",
+    "url": "/bro-guide/static/js/9.4e4e89f6.chunk.js"
+  },
+  {
+    "revision": "f18e5fe50b72fea24068",
+    "url": "/bro-guide/static/js/main.2e6505f7.chunk.js"
+  },
+  {
+    "revision": "74f557b2ed225242ac2b",
+    "url": "/bro-guide/static/js/polyfills-dom.d9ba684e.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/bro-guide/static/js/polyfills-dom.d9ba684e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf584f63fb4311b13da2",
+    "url": "/bro-guide/static/js/runtime-main.b1f105e8.js"
+  },
+  {
+    "revision": "c92c40ab7f5d91e09add",
+    "url": "/bro-guide/static/js/stencil-polyfills-css-shim.d0d7f458.chunk.js"
+  },
+  {
+    "revision": "4d74d7660ac47786278d",
+    "url": "/bro-guide/static/js/stencil-polyfills-dom.dfff4db0.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/bro-guide/static/js/stencil-polyfills-dom.dfff4db0.chunk.js.LICENSE.txt"
+  }
+]);
